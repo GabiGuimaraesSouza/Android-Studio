f7eb7ca038b9d6c944b7a8b706fdc3f40c643e3d
GabiGuimaraesSouza <guimaraes.gabi.souza@gmail.com>
2022-10-13 08:26:10 -0300

    Todo 4,5,6 e 7 + atualização room

---
+++ Todo Final/app/src/main/java/com/example/todo_example/api/Repository.kt
@@ -0,0 +1,20 @@
+package com.example.todo_example.api
+
+import com.example.todo_example.model.Categoria
+import com.example.todo_example.model.Tarefa
+import retrofit2.Response
+
+class Repository {
+
+    suspend fun listCategoria(): Response<List<Categoria>>{
+        return RetrofitInstance.api.listCategoria()
+    }
+
+    suspend fun addTarefa(tarefa: Tarefa): Response<Tarefa>{
+        return RetrofitInstance.api.addTarefa(tarefa)
+    }
+
+    suspend fun listTarefa(): Response<List<Tarefa>>{
+        return RetrofitInstance.api.listTarefa()
+    }
+}
